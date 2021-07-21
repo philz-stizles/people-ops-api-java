3599c559b5cd6e05802ad4dc5d211b46832f640b
philz-stizles <theophilusighalo@gmail.com>
2021-07-21 23:16:19 +0100

    configure spring boot- spring data rest

--- src/main/java/io/devdezyn/peopleops/repository/EmployeeRepository.java
+++ src/main/java/io/devdezyn/peopleops/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package io.devdezyn.peopleops.repository;
 
 import io.devdezyn.peopleops.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="employees")
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
